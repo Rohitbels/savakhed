89fbcf007e321dbc62bd92d8c98d4616b6060566
kundankabra <kundan.21810729@viit.ac.in>
2020-12-25 01:56:10 +0530

    initialCommit

---
+++ src/Components/details.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import './details.css'
+function details() {
+    return (
+        <div className="details_container">
+          
+          <div className="details_back">
+            <a href="#">
+                <button className="back_btn">
+                    Go Back
+                </button>
+            </a>
+          </div>
+          <hr />
+            <div className="details_image">
+            <img src="https://m.media-amazon.com/images/I/51Z0nLAfLmL.jpg" alt="Book Image" className="book_image" />
+            </div>
+            <div className="book_details">
+                <div className="rows">
+                    <span className="label">Book Name</span>
+                    <span className="book_name">The Alchemist</span>
+                </div>
+                <hr />
+                <div className="rows">
+                    <span className="label">Author</span>
+                    <span className="book_name">Paulo Coelho</span>
+                </div>
+                <hr />
+                <div className="rows">
+                    <span className="label">Year Of Release</span>
+                    <span className="book_name">2006</span>
+                </div>
+                <hr />
+                <div className="rows">
+                    <span className="label">Publication</span>
+                    <span className="book_name">The Alchemist</span>
+                </div>
+                <hr />
+                <div className="rows">
+                    <span className="label">Genre</span>
+                    <span className="book_name">The Alchemist</span>
+                </div>
+                <hr />
+            </div>
+        </div>
+    )
+}
+
+export default details
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Details from './Components/details'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Details />
   </React.StrictMode>,
   document.getElementById('root')
 );
