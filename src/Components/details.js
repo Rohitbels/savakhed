2bedc7a3e78bb164edf9da986048c83f4f615e45
kundankabra <kundan.21810729@viit.ac.in>
2020-12-27 20:03:04 +0530

    made responsive

--- src/Components/details.js
+++ src/Components/details.js
@@ -2,16 +2,16 @@ import React from 'react'
 import './details.css'
 function details() {
     return (
-        <div className="details_container">
-          
-          <div className="details_back">
-            <a href="#">
-                <button className="back_btn">
-                    Go Back
-                </button>
-            </a>
-          </div>
-          <hr />
+<div>
+        <div className="details_back">
+        <a href="#">
+            <button className="back_btn">
+                Go Back
+            </button>
+        </a>
+        <hr className="hr"/>
+      </div>
+        <div className="flex-container">
             <div className="details_image">
             <img src="https://m.media-amazon.com/images/I/51Z0nLAfLmL.jpg" alt="Book Image" className="book_image" />
             </div>
@@ -20,29 +20,29 @@ function details() {
                     <div className="label">Book Name</div>
                     <span className="book_name">The Alchemist</span>
                 </div>
-                <hr />
+                <hr className="hr-inLabel"/>
                 <div className="rows">
                     <span className="label">Author</span>
                     <span className="book_name">Paulo Coelho</span>
                 </div>
-                <hr />
+                <hr className="hr-inLabel" />
                 <div className="rows">
                     <span className="label">Year Of Release</span>
                     <span className="book_name">2006</span>
                 </div>
-                <hr />
+                <hr className="hr-inLabel" />
                 <div className="rows">
                     <span className="label">Publication</span>
                     <span className="book_name">The Alchemist</span>
                 </div>
-                <hr />
+                <hr className="hr-inLabel" />
                 <div className="rows">
                     <span className="label">Genre</span>
                     <span className="book_name">The Alchemist</span>
                 </div>
                 <br/>
-                <hr />
             </div>
+        </div>
         </div>
     )
 }
