832085810a5a2e66b91d89fccf9cec5703e471c9
kundankabra <kundan.21810729@viit.ac.in>
2020-12-27 21:37:58 +0530

    thirdCommit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Details from './Components/details'
+import App from './App'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Details />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
