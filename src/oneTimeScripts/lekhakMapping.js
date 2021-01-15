75a9eb5fbc68e050e6db3699824f27b2842a70b3
PranavSarda <pranavsarda147@gmail.com>
2021-01-15 14:14:56 +0530

    LekhakList Script Checkpoint.

--- src/oneTimeScripts/lekhakMapping.js
+++ src/oneTimeScripts/lekhakMapping.js
@@ -1,11 +1,7 @@
-const Fs = require('fs');
-const { format } = require('path');
-
-
+//Firebase Setup
 const firebase = require("firebase/app");
 require("firebase/firestore");
 require("firebase/auth");
-
 const config = {
 	apiKey: "AIzaSyCzHYtN3HUc7uNhG15YD3hrnyiX_poQUrM",
 	authDomain: "devsavakhed.firebaseapp.com",
@@ -15,97 +11,78 @@ const config = {
 	appId: "1:774083254382:web:b184cb1b0851be9474ae7f",
 	measurementId: "G-RCY6EWCX6V",
 };
-
 firebase.initializeApp(config);
-
 const auth = firebase.auth();
 const db = firebase.firestore();
 
-
-const honorifics = ['श्री', 'सौ', 'प्रा', 'ऍड', 'डॉ', 'सीए', 'अनु', 'dr', 'संग्रा'/*row 92*/, 'प्रो', 'संपा'/*4 word arrays*/, 'आचार्य', 'कॅ', 'संक', 'कौ', 'स्वा', 'गुरु', 'कै', 'पं', 'ऍडव्होकेट'];    //
-
-//Return String of lekhakName after removing honorifics from array.
+//Function to convert given array of strings to a proper string after removing honorifics.
+const honorifics = ['श्री', 'सौ', 'प्रा', 'ऍड', 'डॉ', 'सीए', 'अनु', 'dr', 'संग्रा', 'प्रो', 'संपा', 'आचार्य', 'कॅ', 'संक', 'कौ', 'स्वा', 'गुरु', 'कै', 'पं', 'ऍडव्होकेट'];
 function csvArrayToString(csvString) {
     let array = csvString.split(',');
     let i, j;
-    for(i = 0; i < array.length; i++) {
-        for(j = 0; j < honorifics.length; j++) {
+
+    for(i = 0; i < array.length; i++) {             //For each word in array
+        for(j = 0; j < honorifics.length; j++) {    //Check each honorific present
             if(array[i] === honorifics[j]) {
                 array.splice(i, 1);
-                i--;
+                if(i >= array.length)               //i'th element was deleted
+                    break;                          //If no next element, break
+                else
+                    j = 0;                          //Start from first honorific again for new word present at 'i'th location in array of words
             }
         }
     }
     let string = array.join(" ");
-    //if(array.length > 3)
-    //    console.log('A row arrived: ', string, count);
     return string;
 }
 
+//Reading Input from the CSV using csv-reader package
+const Fs = require('fs');
+const { format } = require('path');
+const dict = { }
+const dictToWrite = { }
 const CsvReadableStream = require('csv-reader');
-let inputStream = Fs.createReadStream('src/oneTimeScripts/Recheck - संकीर्ण.csv', 'utf8'); 
-
-const dict = {
-
-}
-const dictToWrite = {
-
-}
-
-let count = 0;
+let inputStream = Fs.createReadStream('src/oneTimeScripts/Recheck - संकीर्ण.csv', 'utf8');
 inputStream
     .pipe(new CsvReadableStream({ parseNumbers: true, parseBooleans: true, trim: true }))
     .on('data', function (row) {
-        
-        count++;
-        let firstChar = csvArrayToString(row[0])[0];
-
+        let nameString = csvArrayToString(row[0]);
+        let firstChar = nameString[0];
         if(dict[firstChar] && dict[firstChar].length) {
-            if(dict[firstChar].indexOf(csvArrayToString(row[0])) === -1)
-                dict[firstChar].push(csvArrayToString(row[0]));
-                //dict[firstChar].push(csvArrayToString(row[0]));
+            if(dict[firstChar].indexOf(nameString) === -1)
+                dict[firstChar].push(nameString);
         } 
         else {
-            dict[firstChar] = [csvArrayToString(row[0])]
+            dict[firstChar] = [nameString];
         }
-
     })
     .on('end', function (data) {
-        //console.log(Object.keys(dict));
-        
         for (const [key, value] of Object.entries(dict)) {
+            //-----------Uncomment for writing into CSV----------------
+            //dictToWrite[key] = value.toString();              //Dict for writing to csv, has different format from our dict
 
-            dictToWrite[key] = value.toString();              //Dict for writing csv
-            //console.log(key + " " + value.length);
+            //Writing to Firebase
             const dataTemp = {
                 char: key,
                 names: value
             }
-            //console.log(dataTemp.char, dataTemp.names.length);
             if(dataTemp.char === 'r') {         //Remove condition if you want to write to firebase all characters
                 db.collection('lekhakMapping').doc(dataTemp.char).set(dataTemp).then(console.log("Written to Firebase"));
+                //const unionRes = await washingtonRef.update({
+                //    regions: firebase.firestore.FieldValue.arrayUnion('greater_virginia')
+                //});                  
             }
         }
-        //console.log(dictToWrite);
-
-        /*
-        //Writing to CSV File
-        console.log("Start write");
-
+        /*-----------Uncomment for writing into CSV----------------
         const createCsvWriter = require('csv-writer').createObjectCsvWriter;
         const csvWriter = createCsvWriter({
             path: 'src/oneTimeScripts/Write - संकीर्ण.csv',
             header: ['र', 'उ', 'श', 'ग', 'क', 'ज', 'म', 'भ', 'ड', 'व', 'स', 'प', 'इ', 'अ', 'आ', 'ह', 'ए', 'द', 'ब', 'न', 'ख', 'त', 'च', 'ल', 'छ', 'य', 'फ', 'ओ', 'घ', 'ध', 'ऍ', 'n', 'd']
         });
-
         records = [dictToWrite]
         csvWriter.writeRecords(records)       // returns a promise
             .then(() => {
                 console.log('...Done');
             });
-        //CSV Writer end
         */
-        
     });
-
-//head : ['अ', 'आ', 'इ', 'ई', 'उ', 'ऊ', 'ऋ', 'ऌ', 'ऍ', 'ऎ', 'ए', 'ऐ', 'ऑ', 'ऒ', 'ओ', 'औ', 'क', 'ख', 'ग', 'घ', 'ङ', 'च', 'छ', 'ज', 'झ', 'ञ', 'ट', 'ठ', 'ड', 'ढ', 'ण', 'त', 'थ', 'द', 'ध', 'न', 'ऩ', 'प', 'फ', 'ब', 'भ', 'म', 'य', 'र', 'ऱ', 'ल', 'ळ', 'ऴ', 'व', 'श', 'ष', 'स', 'ह', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
