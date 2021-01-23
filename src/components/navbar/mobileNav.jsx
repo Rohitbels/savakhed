e94398c991a8212ae6f4a9e437ec0121e2708140
kundankabra <kundan.21810729@viit.ac.in>
2021-01-23 21:00:59 +0530

    made toggle states to change background color on selecting in bottom navbar

--- src/components/navbar/mobileNav.jsx
+++ src/components/navbar/mobileNav.jsx
@@ -21,19 +21,19 @@ export default class mobileNav extends Component {
         return (
             <div className="mobileNav">
                 {/* search */}
-                <div className={this.state.isClicked === 1 ? "mobileNav_search" : ""}>
+                <div className={this.state.isClicked === 1 ? "mobileNav_clicked" : "mobileNav_unclicked"}>
                     <a href="#/search" onClick={() => this.setState({ isClicked: 1 })}>
                         <img src={searchSvg} className="searchSvg" alt="Search Books" />
                     </a>
                 </div>
                 {/* lekhak list */}
-                <div className={this.state.isClicked === 2 ? "mobileNav_search" : ""}>
+                <div className={this.state.isClicked === 2 ? "mobileNav_clicked" : "mobileNav_unclicked"}>
                     <a href="#/lekhaklist" onClick={() => this.setState({ isClicked: 2})}>
                         <img src={authorSvg2} className="lekhakSvg" alt="Lekhak List" />
                     </a>
                 </div>
                 {/* about */}
-                <div className={this.state.isClicked === 3 ? "mobileNav_search" : ""}>
+                <div className={this.state.isClicked === 3 ? "mobileNav_clicked" : "mobileNav_unclicked"}>
                     <a href="#" onClick={() => this.setState({ isClicked: 3})}>
                         <img src={aboutSvg} className="lekhakSvg" alt="About US" />
                     </a>
