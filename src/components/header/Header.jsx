2863364f6f5833e93dfd2ca2574aaeedf6d08ecb
PurujeetPatil <patilpurujeet@gmail.com>
2021-01-23 21:09:07 +0530

    Updated `Header`  component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./header.css";
 // import { ReactComponent as Arrow } from "./caret.svg";
 
 function Header() {
-	const label = (href) => {
-		if (href.includes("details")) return "Details";
-		if (href.includes("search")) return "Search";
-		return "Search";
+	
+	const setLabel = () => {
+		const href = window.location.href;
+		if (href.includes("lekhak-list")) return "lekhak-list";
+		if (href.includes("about-us")) return "about-us";
+		if (href.includes("details")) return "details";
+		if (href.includes("search")) return "search";
+		return "search"
 	};
+	const [tab, setTab] = useState(setLabel)
+	const [prevTab, setPrevTab] = useState()
+
+	const changeCSS = () => {
+		document.getElementById(tab).classList.add("active");
+	}
+
+	useEffect(() => document.getElementById(tab).classList.add("active"), [])
 
 	return (
 		<nav className="nav-bar">
@@ -16,7 +28,22 @@ function Header() {
 					<button>&lt;</button>
 				</a>
 			) : null}
-			<a href="/search">{label(window.location.href)}</a>
+			{/* <a href="/search">{label(window.location.href)}</a> */}
+			<a href="/search" onClick={() => {setPrevTab(tab);setTab("search")}}>
+				<div className="link-placeholders" id="search">
+					Search
+				</div>
+			</a>
+			<a href="/lekhak-list" onClick={() => {setPrevTab(tab);setTab("lekhak-list")}}>
+				<div className="link-placeholders" id="lekhak-list">
+						Lekhak List
+				</div>
+			</a>
+			<a href="/about-us" onClick={() => {setPrevTab(tab);setTab("about-us")}}>
+				<div className="link-placeholders" id="about-us">
+					About Us
+				</div>
+			</a>
 		</nav>
 	);
 }
