a2426fa8fb0c69fc3d1afa16efa3c272acd08097
kundankabra <kundan.21810729@viit.ac.in>
2021-01-23 20:24:38 +0530

    made toggle states to change background color on selecting in bottom navbar

--- src/components/navbar/mobileNav.jsx
+++ src/components/navbar/mobileNav.jsx
@@ -5,17 +5,33 @@ import authorSvg from "../../svg/author.svg"
 import authorSvg2 from "../../svg/author2.svg"
 import aboutSvg from "../../svg/about.svg"
 export default class mobileNav extends Component {
+    constructor(){
+        super();
+        this.state = {
+            isSearchClicked : true,
+            isLekhakClicked : false,
+            isAboutClicked : false
+        }
+    }
+
+
     render() {
         return (
             <div className="mobileNav">
-                <div className="mobileNav_search">
-                <a href="#/search"><img src={searchSvg} className="searchSvg" alt="Search Books"/></a>
+                <div className={this.state.isSearchClicked === true ? "mobileNav_search" : ""}>
+                <a href="#/search" onClick={() => this.setState({isSearchClicked:true})} onBlur={() => this.setState({isSearchClicked:false})}>
+                    <img src={searchSvg} className="searchSvg" alt="Search Books"/>
+                </a>
                 </div>
-                <div className="mobileNav_lekhaklist">
-                <a href="#/lekhaklist"><img src={authorSvg2} className="lekhakSvg" alt="Lekhak List"/></a>
+                <div className={this.state.isLekhakClicked === true ? "mobileNav_search" : ""}>
+                <a href="#/lekhaklist" onClick={() => this.setState({isLekhakClicked:true, isSearchClicked:false})} onBlur={() => this.setState({isLekhakClicked:false})}>
+                    <img src={authorSvg2} className="lekhakSvg" alt="Lekhak List"/>
+                </a>
                 </div>
-                <div className="mobileNav_aboutus">
-                <a href="#"><img src={aboutSvg} className="lekhakSvg" alt="About US"/></a>
+                <div className={this.state.isAboutClicked === true ? "mobileNav_search" : ""}>
+                <a href="#" onClick={() => this.setState({isAboutClicked:true, isSearchClicked:false})} onBlur={() => this.setState({isAboutClicked:false})}>
+                    <img src={aboutSvg} className="lekhakSvg" alt="About US"/>
+                </a>
                 </div>
             </div>
         )
