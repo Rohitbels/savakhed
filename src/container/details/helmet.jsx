9c005b9c8bb6e4cb105c88c93af4d98e109dde3e
kundankabra <kundan.21810729@viit.ac.in>
2021-02-19 02:08:42 +0530

    trial for helmet

--- src/container/details/helmet.jsx
+++ src/container/details/helmet.jsx
@@ -12,29 +12,17 @@ export default function HelmetMetaData(props) {
     return (
         <Helmet>
             <meta property="og:title" content={title} />
-            <meta property="og:image" content="https://w7.pngwing.com/pngs/808/1018/png-transparent-e-book-computer-icons-reading-book-icon-angle-reading-logo-thumbnail.png" />
+         <meta property="og:image" content="https://w7.pngwing.com/pngs/808/1018/png-transparent-e-book-computer-icons-reading-book-icon-angle-reading-logo-thumbnail.png" />
+            <meta content="image/*" property="og:image:type" />
             <title>{title}</title>
             <meta charset="utf-8" />
-            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-            <meta name="csrf_token" content="" />
-            <meta property="type" content="website" />
             <meta property="url" content={currentUrl} />
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-            <meta name="theme-color" content="#ffffff" />
-            <meta name="_token" content="" />
-            <meta name="robots" content="noodp" />
             <meta property="title" content={title} />
             <meta property="quote" content={quote} />
-            <link rel="apple-touch-icon" href="" data-react-helmet="true" />
+            {/* <link rel="apple-touch-icon" href="" data-react-helmet="true" /> */}
             <meta name="description" content={title} />
-            <meta rel="icon" href="https://w7.pngwing.com/pngs/808/1018/png-transparent-e-book-computer-icons-reading-book-icon-angle-reading-logo-thumbnail.png" />
-            <meta property="image" content="https://w7.pngwing.com/pngs/808/1018/png-transparent-e-book-computer-icons-reading-book-icon-angle-reading-logo-thumbnail.png" />
-            <meta property="og:locale" content="en_US" />
-            <meta property="og:type" content="website" /> 
-            <meta content="image/*" property="og:image:type" />
+            {/* <meta rel="icon" href="https://w7.pngwing.com/pngs/808/1018/png-transparent-e-book-computer-icons-reading-book-icon-angle-reading-logo-thumbnail.png" /> */}
             <meta property="og:url" content={currentUrl} />
-            <meta property="og:site_name" content="Book details" />
-            <meta property="og:description" content={description} />
             <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
         </Helmet>
     );
