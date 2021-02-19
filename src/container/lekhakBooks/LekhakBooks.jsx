37c80ac88cd67c9d1c3029154b1bc2b19e6f0690
PranavSarda <pranavsarda147@gmail.com>
2021-02-19 22:56:51 +0530

    Images of authors being fetched. Shown in raw HTML.

--- src/container/lekhakBooks/LekhakBooks.jsx
+++ src/container/lekhakBooks/LekhakBooks.jsx
@@ -9,103 +9,21 @@ import './LekhakBooks.css'
  *  getImgURL(query) --> downloadFile(url, filename) --> upload(file, filename) --> update(url)
  * 	Only getImgURL needs to be called with query
  ***********************************************/
-
 import { storage } from "../../firebase";
-
 const storageRef = storage.ref();
-
 const API_KEY = "AIzaSyB1TtjgdaS-JyFVHFmWz_OMXhg8ft5Tbpw";
 const AUTHORS_ENGINE = "af35ce6be8762aee9";
 const STORAGE_LOCATION = "authors";
 
-// Updating firestore with downloadURL
-// Make necessary changes here for uploading -------------------------------------------------------------
-const update = async (url) => {
-	console.log("update function() : " + url)
-    //const docUpdate = await collection.doc(docRef).update({ imageURL: url });
-};
-
-// Uploading to storage
-const upload = (file, filename) => {
-	var metadata = {
-		contentType: "image/jpg",
-	};
-
-	let uploadTask = storageRef
-		.child(`${STORAGE_LOCATION}/${filename.replaceAll(" ", "_")}`)
-		.put(file, metadata);
-
-	uploadTask.on(
-		"state_changed",
-		(snapshot) => {
-			var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-		},
-		(error) => {
-			console.log(error);
-		},
-		() => {
-			uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-				// downloadURL is URL to access image from storage
-				console.log("File available at", downloadURL);
-				update(downloadURL); // Function Call
-			});
-		}
-	);
-};
-
-// Convert the image into blob for uploading
-const downloadFile = (url, filename) => {
-	var xhr = new XMLHttpRequest();
-	xhr.responseType = "blob";
-	xhr.onload = function () {
-		console.log(xhr.response);
-		upload(xhr.response, filename); // Function Call
-	};
-	xhr.open("GET", url);
-	xhr.send();
-};
-
-// Fetch Image URL from custom search
-const getImgURL = (query) => {
-	const xhr = new XMLHttpRequest();
-	let url = "";
-	xhr.addEventListener("load", () => {
-		const json = JSON.parse(xhr.responseText);
-		const { items = [] } = json;
-
-		try {
-			// If it finds Image URL
-			url = items[0].image.thumbnailLink; // Temporary image url from search results
-			console.log("URL result : ", url);
-			downloadFile(url, query); //Function call
-		} catch (error) {
-			// If it doesnt find Image URL
-			console.log("image set to default");
-			console.log(error);
-		}
-	});
-	xhr.open(
-		"GET",
-		`https://customsearch.googleapis.com/customsearch/v1/siterestrict?searchType=image&cx=${AUTHORS_ENGINE}&q=${encodeURI(
-			query
-		)}&key=${API_KEY}`
-	);
-	xhr.send();
-};
-
-/*******************************************************
- * End of Image URL Functions
- *******************************************************/
 
 class LekhakBooks extends Component {
     constructor(props) {
         super(props);
-
         this.state = {
             lekhakSearched: this.props.lekhakSearched,
             lekhakResults: this.props.lekhakResults,
             currentLekhak: this.props.currentLekhak,
-            lekhakDict: this.props.lekhakDict,
+            imgURL: ""
         };
     }
 
@@ -136,14 +54,97 @@ class LekhakBooks extends Component {
         //const bookNamesArray = doc.data().lekhakNameJoint;
     }
 
-    componentDidMount() {
+    //Img URL Function
+    getImgURL = (query, selfObj) => {
+        const xhr = new XMLHttpRequest();
+        let url = "";
+        xhr.addEventListener("load", () => {
+            const json = JSON.parse(xhr.responseText);
+            const { items = [] } = json;
+
+            try {
+                // If it finds Image URL
+                url = items[0].image.thumbnailLink; // Temporary image url from search results
+                //console.log("URL result : ", url);
+            
+                var xhrDownload = new XMLHttpRequest();
+                xhrDownload.responseType = "blob";
+                xhrDownload.onload = function () {
+                    //File Upload to Firebase Storage
+                    var metadata = {
+                        contentType: "image/jpg",
+                    };
+                    let uploadTask = storageRef
+                        .child(`${STORAGE_LOCATION}/${query.replaceAll(" ", "_")}`)
+                        .put(xhrDownload.response, metadata);
+                    uploadTask.on(
+                        "state_changed",
+                        (snapshot) => {
+                            //var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                        },
+                        (error) => {
+                            console.log(error);
+                        },
+                        () => {
+                            uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+                                // downloadURL is URL to access image from storage
+                                //console.log("File available at", downloadURL);
+
+                                //console.log("update function() : " + downloadURL)
+                                //console.log(selfObj);
+
+                                //console.log(selfObj.props.lekhakDict[selfObj.props.currentLekhak]);
+                                let currLekhakDict = selfObj.props.lekhakDict;
+                                currLekhakDict[selfObj.props.currentLekhak].imgURL = downloadURL;
+                                //console.log(currLekhakDict[selfObj.props.currentLekhak])
+                                
+                                //console.log("Firebase in Temp Function");
+                                db.collection("newMappingStructure")
+                                    .doc(selfObj.props.isBtnClicked)
+                                    .update({
+                                        names: currLekhakDict,
+                                    })
+                                    .then(function () {
+                                        selfObj.props.setParentState({ lekhakDict : currLekhakDict });
+                                        console.log("Written to new collection in Firebase from trial function." + selfObj.props.currentLekhak);
+                                        console.log(selfObj.props);
+                                    });
+
+                                // update(downloadURL); // Function Call
+                            });
+                        }
+                    );
+
+                };
+                xhrDownload.open("GET", url);
+                xhrDownload.send();
+            
+            } catch (error) {
+                // If it doesnt find Image URL
+                console.log("image set to default");
+                console.log(error);
+            }
+        });
+        xhr.open(
+            "GET",
+            `https://customsearch.googleapis.com/customsearch/v1/siterestrict?searchType=image&cx=${AUTHORS_ENGINE}&q=${encodeURI(
+                query
+            )}&key=${API_KEY}`
+        );
+        xhr.send();
+    };
+
+    /*******************************************************
+     * End of Image URL Functions
+     *******************************************************/
+
+    async componentDidMount() {
         this.props.setParentState({ lekhakSearched: this.props.lekhakSearched });
-        console.log("Current LekhakDict" + this.props.lekhakDict);
-        // if(this.props.lekhakDict[this.state.currentLekhak].imgURL === undefined) {
-        //     //Query the img
-        //     getImgURL(this.props.currentLekhak);
-        // }
-        // console.log(this.props);
+        //console.log(this);
+        //console.log(this.props.lekhakDict[this.props.currentLekhak].imgURL);
+        if(this.props.lekhakDict[this.props.currentLekhak].imgURL === undefined) {
+            await this.getImgURL(this.props.currentLekhak, this);
+        }
     }
 
     render() {
@@ -156,17 +157,17 @@ class LekhakBooks extends Component {
             <div>
                 <div className="lekhakBooklist">
                     <h1>लेखक : { this.props.currentLekhak }</h1>
-                    {/* <div className="author_img">
-                        {this.props.lekhakDict[this.state.currentLekhak].imgURL !== undefined ?
-                            <img src={this.props.lekhakDict.currentLekhak.imgURL} alt="author image" className="author_img_class"/>
-                        :<div></div>
-                        }
-                    </div> */}
                     <ListSection
                         setCurrentDetails={this.props.setCurrentDetails}
                         tableElements={this.props.lekhakResults}
                         searched={true}
                     />
+                </div>
+                <div className="author_img">
+                    {this.props.lekhakDict[this.props.currentLekhak].imgURL !== "" || this.props.lekhakDict[this.props.currentLekhak].imgURL !== undefined ?
+                        <img src={this.props.lekhakDict[this.props.currentLekhak].imgURL} alt="" className="author_img_class"/>
+                    :<div></div>
+                    }
                 </div>
             </div>
         )
